50b2fabfa3ffd5bc6a26b76425b3482bfbbd6f2b
jawadAzx <23100321@lums.edu.pk>
2022-12-25 16:22:14 +0500

    contract deployed

--- deverse/src/utils/constants.js
+++ deverse/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Maincontract.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = "0x3D2540e4be61eaDca5A5E69C8ed52b5c6b217516";
+export const contractAddress = "0x70785cBF79aBE8ae220212bF31BEA0561ad9d843";
