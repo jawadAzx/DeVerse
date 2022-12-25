58da9b17b2363a959c41a52eeacef08dd829ce12
jawadAzx <23100321@lums.edu.pk>
2022-12-25 16:26:49 +0500

    ready for deployment

--- smartcontract/migrations/1_deverse_migration.js
+++
@@ -1,6 +0,0 @@
-const Maincontract = artifacts.require("Maincontract");
-// const UserContract = artifacts.require("UserContract");
-module.exports = function (deployer) {
-    deployer.deploy(Maincontract);
-    // deployer.deploy(UserContract);
-}
